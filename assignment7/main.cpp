2f8ecd78345f2d68440c10caf6628e91f4aa3fa0
lnkerr <754200735@qq.com>
2025-06-29 00:39:06 +0800

    assign7

--- assignment7/main.cpp
+++ assignment7/main.cpp
@@ -52,7 +52,14 @@ template <typename T> struct ListNode {
  */
 template <typename T> cs106l::unique_ptr<ListNode<T>> create_list(const std::vector<T>& values) {
   /* STUDENT TODO: Implement this method */
-  throw std::runtime_error("Not implemented: createList");
+  //throw std::runtime_error("Not implemented: createList");
+  cs106l::unique_ptr<ListNode<T>> head = nullptr;
+  for (auto it = values.rbegin(); it != values.rend(); ++it) {
+    cs106l::unique_ptr<ListNode<T>> new_node = cs106l::make_unique<ListNode<T>>(*it);
+    new_node->next = std::move(head); // Move the current head to the new node's next
+    head = std::move(new_node); // Update head to point to the new node
+  }
+  return head;
 }
 
 /**
